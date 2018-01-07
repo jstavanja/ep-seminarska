c02bdf44e8c4c49ba776a7522e4083b723d07a5f
jstavanja <jakastavanja@gmail.com>
2018-01-07 20:13:17 +0100

    Initial commit :ok_hand:

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+require_once("controller/StoreController.php");
+
+define("BASE_URL", $_SERVER["SCRIPT_NAME"] . "/");
+define("IMAGES_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/images/");
+define("CSS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/css/");
+
+$path = isset($_SERVER["PATH_INFO"]) ? trim($_SERVER["PATH_INFO"], "/") : "";
+
+// ROUTER: defines mapping between URLS and controllers
+$urls = [
+    "store" => function () {
+        StoreController::index();
+    },
+    "login" => function () {
+        LoginController::index();
+    },
+    "registration" => function () {
+        RegistrationController::index();
+    },
+    "administrator" => function () {
+        AdministratorController::index();
+    },
+    "customer" => function () {
+        CustomerController::index();
+    },
+    "seller" => function () {
+        SellerController::index();
+    },
+    "" => function () {
+        ViewHelper::redirect(BASE_URL . "store");
+    },
+];
+    
+try {
+    if (isset($urls[$path])) {
+        $urls[$path]();
+    } else {
+        echo "No controller for '$path'";
+    }
+} catch (InvalidArgumentException $e) {
+    ViewHelper::error404();
+} catch (Exception $e) {
+    echo "An error occurred: <pre>$e</pre>";
+} 
+
