4f0282ba5e437b951f1c03e2a9ac595682f38d47
Blaž <blaz.rupnik56@gmail.com>
2018-01-08 21:37:43 +0100

    function that will update data for user

--- controller/CustomerController.php
+++ controller/CustomerController.php
@@ -19,4 +19,28 @@ class CustomerController {
             "title" => "Store :: Nadzorna plošča uporabnika"
         ]);
     }
+
+    public static function editOwnData(){
+        echo ViewHelper::render("view/customer.php", [
+            "title" => "Store :: Nadzorna plošča uporabnika"
+        ]);
+    }
+
+    private static function checkValues($input) {
+        if (empty($input)) {
+            return FALSE;
+        }
+
+        if ($input['password'] != $input['password-repeat']) {
+            return FALSE;
+        }
+
+        $result = TRUE;
+        foreach ($input as $value) {
+            var_dump($value);
+            $result = $result && $value != false;
+        }
+
+        return $result;
+    }
 }
--- index.php
+++ index.php
@@ -31,6 +31,12 @@ $urls = [
         }
         else ViewHelper::error404();
     },
+    "customer/editOwnData" => function() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            CustomerController::editOwnData();
+        }
+        else ViewHelper::error404();
+    },
     "login/log_user_in" => function () {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             LoginController::log_user_in();
