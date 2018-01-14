18d6fe4ba995e9d3467bc6497bad5bae9ef11c56
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2018-01-14 16:06:47 +0100

    small redirect base_url bug fix

--- static/js/order-preview.js
+++ static/js/order-preview.js
@@ -5,7 +5,7 @@ window.onload = () => {
     axios.post('/cart/completeOrder')
     .then((res) => {
       if (res.data.success) {
-        document.querySelector(".card").innerHTML = "<h3 class=\"is-success animated fadeIn bounce\">Uspešno ste oddali naročilo. </h3><a href=\"/\">Nazaj na domačo stran.</a>"
+        document.querySelector(".card").innerHTML = "<h3 class=\"is-success animated fadeIn bounce\">Uspešno ste oddali naročilo. </h3><a href=\"" + base_url + "\">Nazaj na domačo stran.</a>"
       }
     })
   })
